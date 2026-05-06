8343302e420e3ec3116c693086c8160a6a1116a7
Amanda Walshjamin <amanda.jamin@gmail.com>
2026-05-06 15:28:48 -0400

    red flag error support

--- backend/src/main/java/org/hobynye/thankyoumatcher/model/Candidate.java
+++ backend/src/main/java/org/hobynye/thankyoumatcher/model/Candidate.java
@@ -1,18 +1,27 @@
 package org.hobynye.thankyoumatcher.model;
 
-import java.util.List;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@AllArgsConstructor
 public class Candidate {
+
     private Student student;
     private Thankable thankable;
     private List<String> reasons;
+    private int cost;
+    private boolean redAlert;
+    private String alertMessage;
 
     public Candidate(Student student, Thankable thankable, List<String> reasons) {
         this.student = student;
         this.thankable = thankable;
         this.reasons = reasons;
+        this.cost = 0;
+        this.redAlert = false;
+        this.alertMessage = null;
     }
 }
